a8a086404aeaa31974447d26fc27d2f86e01b084
Michu <michal.rezerwujsport@gmail.com>
2019-11-02 17:11:54 +0100

    added error handling

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ const peoples = document.getElementById("characters");
 const images = ['./img/Luke.jpg', './img/C-3PO.png', './img/r2d2.jpg', './img/Darth_Vader.png', './img/Leia_Organa.png', './img/Owen.jpg', './img/Beru.jpg', './img/R5-D4.jpg', './img/Biggs.png', './img/obi.jpg'];
 
 async function fetchData() {
+
     const response = await fetch('https://swapi.co/api/people/')
-    const data = await response.json()
+                    .then(res => res.json())
+                    .catch(err => console.log(err));
 
+        let peopleList = document.querySelector('ul');
 
-    let peopleList = document.querySelector('ul');
-
-
-    data.results.forEach((e, i) => {
+        response.results.forEach((e, i) => {
 
         let people = document.createElement('li');
         let image = document.createElement('img');
         let item = document.createElement('div');
 
 
-        people.innerHTML = `<h3>${data.results[i].name}</h3>`;
+        people.innerHTML = `<h3>${response.results[i].name}</h3>`;
         image.src = `${images[i]}`;
         image.alt = "";
         item.className = "items";
@@ -26,8 +26,7 @@ async function fetchData() {
         item.appendChild(image);
         item.appendChild(people);
         peopleList.appendChild(item);
-
+        return response;
     })
-
 }
 fetchData();
